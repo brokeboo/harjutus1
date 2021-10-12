aece3173c2eada6f565785bdb9d1aaf69295e44e
ilya <72455399+brokeboo@users.noreply.github.com>
2021-10-12 09:36:56 +0300

    Add files via upload

---
+++ harjutus-master/Harjutus.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harjutus
+{
+    class Harjutus
+    {
+        public static void Main(string[] args)
+        {
+            /*int mult = 1;
+            int sum = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Please enter a number: ");
+                int smt = Convert.ToInt32(Console.ReadLine());
+                sum += smt;
+                mult = smt * mult;
+            }
+            double avg = sum / 5;
+            Console.WriteLine($"NUM AVG = {avg}\nNums SUM = {sum}\nNums multiplied = {mult}");
+            */
+
+
+            string text;
+            do
+            {
+                Console.WriteLine("buy elephant!");
+                Console.Write("Buy: ");
+
+                text = Console.ReadLine();
+            } while (text != "elephant");
+            Console.WriteLine("Elephant");
+        }
+    }
+}
+    
+    
+      
+
+
---
+++ harjutus-master/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harjutus
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("karjuta cml sein");
+            decimal cm1 = Math.Abs(decimal.Parse(Console.ReadLine()));
+            Console.WriteLine("Kirjuta cm2 sein");
+            decimal cm2 = Math.Abs(decimal.Parse(Console.ReadLine()));
+
+            Console.WriteLine("põranda pindala");
+            Console.WriteLine(cm1 + cm2 + "cm2");
+            Console.WriteLine("Kas tahad remondi teha?")
+            string otvet = Console.ReadLine();
+            if(otvet="jah")
+            {
+                Console.WriteLine("kui suur ruud meetri");
+            }
+            else
+
+        }
+    }
+}
